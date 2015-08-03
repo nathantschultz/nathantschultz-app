811f84bfa5ed580a1fb05da94e342aedf79ffbca
Nathan Schultz <nschultz@familysearch.org>
2015-08-03 10:17:33 -0600

    update to use ugly file and route

--- controllers/routes.js
+++ controllers/routes.js
@@ -12,4 +12,9 @@ module.exports = function(app) {
   app.get('/', function(req, res){
     res.render("index", {});
   });
+  app.get('/ugly', function(req, res){
+    res.render("ugly", {});
+  });
+  
+  
 }
